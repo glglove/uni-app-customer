de39f884de7b9dd20433a2198d5f238acf2e79ca
gaol <331319344@qq.com>
2019-11-28 23:07:20 +0800

    update

--- App.vue
+++ App.vue
@@ -11,6 +11,27 @@
 	import getEnterType from '@/utils/miniProSceneType'
 	import { getDeviceApi } from '@/utils/deviceApi.js'
 	import commApi  from '@/api/comm.js'
+	
+	let io = require('./utils/weapp.socket.io.js')
+	
+// 1. 与服务器端建立连接
+        const socket = io.connect("http://localhost:3000");
+
+        // 2. 监听send按钮点击的事件
+        $("#send").click(function(){
+            // 获取输入的信息
+            let message = $("#message").val().trim();
+            // 向服务器端发送信息
+            socket.emit("sentToServer", message);
+        });
+
+        // 3. 获取服务端发送过来的信息
+        socket.on("sendToClient", message => {
+            console.log(message);
+        });
+        /**
+         * 发布订阅(广播), 一端发布, 可以让多端触发
+         */	
 
 	let self = ''
 	export default {
--- api/config.js
+++ api/config.js
@@ -14,18 +14,8 @@
 	//  gateway: "https://locally.uieee.com",
 	//  gateway: "https://service.yaya91.com:8495",
 	//  gateway: "http://daka.natapp1.cc",
-		// gateway: "http://localhost:5000",
-<<<<<<< Updated upstream
-		// gateway: "http://192.168.1.105:5000",      // 这个为本机的ip 地址 端口号 设置的是 5000  主要是 app-plus 进行真机调试时 用 http://localhost:5000 调取接口时会请求失败
-		// gateway: "https://www.gaolongweb.cn:5000",
 		// gateway: "https://47.105.141.184",    // 这个为本机的ip 地址 端口号 设置的是 5000  主要是 app-plus 进行真机调试时 用 http://localhost:5000 调取接口时会请求失败
 		gateway: "https://www.gaolongweb.cn",    // 这个为本机的ip 地址 端口号 设置的是 5000  主要是 app-plus 进行真机调试时 用 http://localhost:5000 调取接口时会请求失败
-=======
-		// gateway: "http://192.168.1.106:5000",      // 这个为本机的ip 地址 端口号 设置的是 5000  主要是 app-plus 进行真机调试时 用 http://localhost:5000 调取接口时会请求失败
-		gateway: "http://192.168.1.106:5000",      // 这个为本机的ip 地址 端口号 设置的是 5000  主要是 app-plus 进行真机调试时 用 http://localhost:5000 调取接口时会请求失败
-		// gateway: "http://47.105.141.184:5000",    // 这个为本机的ip 地址 端口号 设置的是 5000  主要是 app-plus 进行真机调试时 用 http://localhost:5000 调取接口时会请求失败
-		// gateway: "http://www.gaolongweb.cn",    // 这个为本机的ip 地址 端口号 设置的是 5000  主要是 app-plus 进行真机调试时 用 http://localhost:5000 调取接口时会请求失败
->>>>>>> Stashed changes
 		// gateway: "https://www.kaoyandaka.com",
 	//  mobile: "https://www.kaoyandaka.com",
 	//  resource: "https://www.kaoyandaka.com",
